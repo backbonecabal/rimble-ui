055a38f619f0d1e3208c69e7696bb73e80259dc7
Gustavo Esquinca <g.esq.ca@gmail.com>
2020-03-10 15:14:19 -0400

    add Field stories for new props

--- storybook/stories/Field.stories.js
+++ storybook/stories/Field.stories.js
@@ -6,16 +6,32 @@ export default {
 }
 
 export const field = () => (
-  <Box>
-    <Box>
-      <Field label="Public address">
-        <Input required={true} placeholder={'0x000000'} />
-      </Field>
-    </Box>
-    <Box>
-      <Field label="Email address">
-        <Input type="email" placeholder={'name@example.com'} />
-      </Field>
-    </Box>
-  </Box>
+  <Field label="Email address">
+    <Input type="email" placeholder={'name@example.com'} />
+  </Field>
+)
+
+export const fieldRequired = () => (
+  <Field label="Public address">
+    <Input required={true} placeholder={'0x000000'} />
+  </Field>
+)
+
+export const fieldWithHelpText = () => (
+  <Field
+    label="Email address"
+    helpText={'Please enter your email address.'}
+  >
+    <Input type="email" placeholder={'name@example.com'} />
+  </Field>
+)
+
+
+export const fieldWithError = () => (
+  <Field
+    label="Email address"
+    error={'This field is required.'}
+  >
+    <Input type="email" placeholder={'name@example.com'} />
+  </Field>
 )
